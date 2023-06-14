5832ee6855e32a268a31bc499a327f96d3b7a04f
Delci de Col <delcidecol@gmail.com>
2023-06-13 22:42:17 -0300

    Project created

---
+++ src/main/java/com/eeco/splintteste/SplinttesteApplication.java
@@ -0,0 +1,13 @@
+package com.eeco.splintteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplinttesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplinttesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eeco/splintteste/SplinttesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eeco.splintteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplinttesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
