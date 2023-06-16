4d6931b5ff9e22c37afc9f79ca9f41c991f41738
Delci de Col <delcidecol@gmail.com>
2023-06-16 15:12:52 -0300

    Many-to-Many association with JoinTable

--- src/main/java/com/eeco/sprintteste/entities/Product.java
+++ src/main/java/com/eeco/sprintteste/entities/Product.java
@@ -9,8 +9,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
@@ -29,7 +31,11 @@ public class Product implements Serializable {
 	// foi instanciado a categoria para garantir que a mesma não inicie nula
 	// porque foi usado o hashSet - porque usamos o SET - como é o caso do list e
 	// arraylist
-	@Transient
+	
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", 
+	joinColumns = @JoinColumn(name = "product_id"),
+	inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 
 	public Product() {
