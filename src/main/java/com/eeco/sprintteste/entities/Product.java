af2cea2c1338f889f92a8d436f4188223789ad1b
Delci de Col <delcidecol@gmail.com>
2023-06-20 13:57:32 -0300

    Product-OrdemItem one-to-many association

--- src/main/java/com/eeco/sprintteste/entities/Product.java
+++ src/main/java/com/eeco/sprintteste/entities/Product.java
@@ -12,6 +12,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -37,6 +38,9 @@ public class Product implements Serializable {
 	joinColumns = @JoinColumn(name = "product_id"),
 	inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
+	
+	@OneToMany(mappedBy = "id.product")
+	public Set<OrderItem> items = new HashSet<>();
 
 	public Product() {
 	}
@@ -93,10 +97,22 @@ public class Product implements Serializable {
 	public Set<Category> getCategories() {
 		return categories;
 	}
+	
+	
+	public Set<Order> getOrders(){
+		Set<Order> set = new HashSet<>();
+		for(OrderItem x : items) {
+			set.add(x.getOrder());
+		}
+		return set;
+	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;  //Objects.hash(id);
 	}
 
 	@Override
