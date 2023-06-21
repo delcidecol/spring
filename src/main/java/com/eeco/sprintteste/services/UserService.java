0804acf9c5ec24edfa3b50c47211bcdb281adf3b
Delci de Col <delcidecol@gmail.com>
2023-06-21 17:16:00 -0300

    Exception handling - delete

--- src/main/java/com/eeco/sprintteste/services/UserService.java
+++ src/main/java/com/eeco/sprintteste/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.eeco.sprintteste.entities.User;
 import com.eeco.sprintteste.repositories.UserRepository;
+import com.eeco.sprintteste.services.exceptions.DatabaseException;
 import com.eeco.sprintteste.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -33,8 +36,13 @@ public class UserService {
 
 	// operção para deletar do banco de dados um usuário
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
-
+		}catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
 	// atualização de usuário
